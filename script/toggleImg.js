5d4bc4f9dfdb22c1bb9f141884dcdb74c500f3da
Gabriel Alves <alvezinho.gabs@gmail.com>
2022-03-18 12:14:06 -0300

    Conserto
    Consertando problemas com a lógica do JS, aplicando animações nos elementos

--- script/toggleImg.js
+++ script/toggleImg.js
@@ -2,22 +2,33 @@
 const btnAvanco = document.getElementById('frente');
 const btnVoltar = document.getElementById('voltar');
 
+const projetos = document.querySelectorAll('.container .box')
+
 btnAvanco.addEventListener('click', function(){
-    const projetos = document.querySelector('.container').children;
-    
-
-    let i;
-    for (i = 0; i < projetos.length; i++){
-        
-        if (projetos[i].classList.contains('active')){
-
-            projetos[i].classList.remove('active')
+    for (elemento in projetos){
+        if(projetos[elemento].nextElementSibling === null){
+            projetos[elemento].classList.remove('active')
+            projetos[0].classList.add('active')
+            break
+        }else if(projetos[elemento].classList.contains('active')){
             
-            projetos[i++].classList.add('active')
+            projetos[elemento].nextElementSibling.classList.add('active')
+            projetos[elemento].classList.remove('active')
+            break
         }
-        
     }
-    
-    
+})
 
+btnVoltar.addEventListener('click', function(){
+    for (elemento in projetos){
+        if(projetos[0].classList.contains('active')){
+            projetos[projetos.length-1].classList.add('active')
+            projetos[0].classList.remove('active')
+            break
+        }else if(projetos[elemento].classList.contains('active')){
+            projetos[elemento].previousElementSibling.classList.add('active')
+            projetos[elemento].classList.remove('active')
+            break
+        }
+    }
 })
